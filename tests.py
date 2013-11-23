9c878af883a07ddcff2bf629b9032aa2c58c419d
Damian Myerscough <Damian.Myerscough@gmail.com>
2013-11-23 14:04:15 +0800

    Unittests for test the usage of the Nagios class

--- tests.py
+++ tests.py
@@ -1,37 +1,51 @@
 #!/usr/bin/env python
 
 import unittest
-import os
+
+from NagiosParse import Nagios
 
 class NagiosParseTest(unittest.TestCase):
     def setUp(self):
         self.valid = Nagios('tests/valid.dat')
-        self.invalid = Nagios('tests/invalid.dat')
 
     def test_ParseStatus(self):
         """
         Test to make sure Parsing does occur
         """
-        
+        self.host_block = []
+
+        fh = open('tests/host_options')
+        for i in fh.readlines():
+            self.host_block.append(i.strip())
+
+        # Append the servicestatus block definition since this holds service states
+        self.host_block.append('servicestatus')
+
+        methods = [i for i in dir(self.valid.host('example.com')) if not i.startswith('__')]
+
+        self.assertEqual(tuple(sorted(methods)), tuple(sorted(self.host_block)))
 
     def test_missingBrace(self):
         """
         Parse an invalid cofiguration file
         """
-        pass
+        self.assertRaises(Exception, Nagios, 'tests/invalid.dat')
 
     def test_missingStatus(self):
         """
         Try to read a status file that is missing
         """
-        pass
+        self.assertRaises(Exception, Nagios, 'tests/blah.dat')
 
     def test_queryMethod(self):
         """
         Check to see if the query method return a valid response
         and try query garbage
         """
-        pass
+        methods = ['update_available', 'new_version', 'created',  'last_update_check', 
+                   'last_version', 'version']
+
+        self.assertEqual(tuple(sorted(methods)), tuple(sorted(self.valid.query('info')))) 
 
     def test_hostMethod(self):
         """
